6ebed1f9f677d0bd7ff717885c620125a78c0e08
Andrew Courtice <andrewcourtice@gmail.com>
2016-07-28 12:54:08 +1000

    Added MVC integrations

---
+++ Plug.Web.MVC/PlugDependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Plug.Web.MVC
+{
+    public class PlugDependencyResolver : IDependencyResolver
+    {
+        public Container Container { get; }
+
+        public PlugDependencyResolver(Container container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException(nameof(container));
+            }
+
+            Container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType.IsAbstract || !typeof(IController).IsAssignableFrom(serviceType))
+            {
+                throw new Exception("Invalid controller registration");
+            }
+
+            return Container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Enumerable.Empty<object>();
+        }
+    }
+}
